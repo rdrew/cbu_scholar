8e6ea81e7e12f20105e59badd3cd77ab19a7ac2a
rdrew <robdrewupei@gmail.com>
2017-11-09 15:07:46 -0400

    landing page mostly done

---
+++ templates/components/landing-page/page--front.tpl.php
@@ -0,0 +1,12 @@
+<div class="l-page">
+    <?php print render($page['header']); ?>
+    <div class="l-main">
+        <?php print $messages; ?>
+        <?php print render($page['content']); ?>
+    </div>
+    <!-- above footer -->
+    <div class="grid-container">
+        <?php print render($page['above-footer']); ?>
+    </div>
+    <?php print render($page['footer']); ?>
+</div>

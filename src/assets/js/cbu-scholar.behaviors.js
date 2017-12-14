292a3f2a086cbd76a2600e92e2fceebd8fa9bd7d
rdrew <robdrewupei@gmail.com>
2017-12-14 14:51:53 -0400

    addin filter toolbar

--- src/assets/js/cbu-scholar.behaviors.js
+++ src/assets/js/cbu-scholar.behaviors.js
@@ -2,6 +2,27 @@
 
 
 
+    Drupal.behaviors.searchFilters = {
+        attach: function(context, settings) {
+
+
+            $(elem).each(function (index, value) {
+                var text = $(this).contents().filter(function () {
+                    return this.nodeType === 3;
+                });
+                var link = $(this).contents().filter(function () {
+                    return this.nodeType === 1;
+                });
+                link.empty().prepend(text);
+
+            });
+
+            $( ".remove-filter:contains('=')" ).addClass( "equal" );
+            $( ".remove-filter:contains('≠')" ).addClass( "notequal" );
+
+
+        }
+    };
     Drupal.behaviors.searchFacets = {
         attach: function(context, settings) {
 
@@ -17,20 +38,20 @@
                 .append('<img src="' + minus + '">');
             //More (Expand) or Less (Collapse)
             //$('.categories-menu.menu.nested').each(function(){
-                //var filterAmount = $(this).find('li').length;
-                //if( filterAmount > 5){
-                    //$('li', this).eq(4).nextAll().hide().addClass('toggleable');
-                    //$(this).append('<li class="more">More</li>');
-                //}
+            //var filterAmount = $(this).find('li').length;
+            //if( filterAmount > 5){
+            //$('li', this).eq(4).nextAll().hide().addClass('toggleable');
+            //$(this).append('<li class="more">More</li>');
+            //}
             //});
 
             //$('.categories-menu.menu.nested').on('click','.more', function(){
-                //if( $(this).hasClass('less') ){
-                    //$(this).text('More').removeClass('less');
-                //}else{
-                    //$(this).text('Less').addClass('less');
-                //}
-                //$(this).siblings('li.toggleable').slideToggle();
+            //if( $(this).hasClass('less') ){
+            //$(this).text('More').removeClass('less');
+            //}else{
+            //$(this).text('Less').addClass('less');
+            //}
+            //$(this).siblings('li.toggleable').slideToggle();
             //});
 
 
